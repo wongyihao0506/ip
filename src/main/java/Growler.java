569bacb1cacb17e672b2aa83be84453030a3ff87
WongYiHao <e1156090@u.nus.edu>
2025-01-26 17:17:59 +0800

    Level 1-0

---
+++ src/main/java/Growler.java
@@ -0,0 +1,8 @@
+public class Growler {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Growler \nWhat can I do for you? \n";
+        String Farewell = "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+        System.out.println(Farewell);
+    }
+}
