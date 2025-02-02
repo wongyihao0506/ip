83191de6a9e8d9d03b35a056c60a911c40d6f55e
WongYiHao <e1156090@u.nus.edu>
2025-02-02 14:19:26 +0800

    1-2

--- src/main/java/Growler.java
+++ src/main/java/Growler.java
@@ -1,16 +1,34 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Growler {
+    private static ArrayList<String> list = new ArrayList<>();
     public static void main(String[] args) {
         String greet = "Hello! I'm Growler \nWhat can I do for you? \n";
         String Farewell = "Bye. Hope to see you again soon!";
         System.out.println(greet);
         String userInput;
         Scanner in = new Scanner(System.in);
-        userInput = in.nextLine();
-        if (userInput.equals("bye")) {
-            System.out.println(Farewell);
-        } else {
-            System.out.println(userInput);
+        while (true) {
+            userInput = in.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(Farewell);
+                break;
+            } else if (userInput.equals("list")) {
+                showList();
+            } else {
+                addToList(userInput);
+                System.out.println("added: " + userInput);
+            }
+        }
+        in.close();
+    }
+        private static void addToList(String userInput){
+        list.add(userInput);
+    }
+    private static void showList(){
+        for (int i = 0; i < list.size(); i++){
+            System.out.println((i+1) + ". " + list.get(i));
         }
     }
 }
