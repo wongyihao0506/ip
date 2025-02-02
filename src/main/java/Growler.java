bb64ed2be7d6f5cd5977cc49b90783933faf9e9a
WongYiHao <e1156090@u.nus.edu>
2025-02-02 13:44:46 +0800

    1-1

--- src/main/java/Growler.java
+++ src/main/java/Growler.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class Growler {
     public static void main(String[] args) {
         String greet = "Hello! I'm Growler \nWhat can I do for you? \n";
         String Farewell = "Bye. Hope to see you again soon!";
         System.out.println(greet);
-        System.out.println(Farewell);
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+        if (userInput.equals("bye")) {
+            System.out.println(Farewell);
+        } else {
+            System.out.println(userInput);
+        }
     }
 }
